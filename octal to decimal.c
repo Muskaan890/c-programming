2b7e303e0a0a085e3da2f4b5d42efe70f1a9f7e7
Muskaan890 <muskaanganie@gmail.com>
2025-10-21 21:29:36 +0530

    Update octal to decimal.c

--- octal to decimal.c
+++ octal to decimal.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int num, dec=0, rem, base=1;
+    int octal, dec=0, rem, base=1;
     printf("enter  the octal number:");
-    scanf("%d",&num);
-    while(num>0)
+    scanf("%d",&octal);
+    while(octal>0)
     {
-        rem=num%10;
+        rem=octal%10;
         dec=dec+rem*base;
-        num=num/10;
+        octal=octal/10;
         base= base*8;
         }
         printf("decimal equivalent is :%d",dec);
         return 0;
 }
+
